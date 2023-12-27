d65a0a0ae8fee4a30fe1d048106f80db58782b6b
GGomezMorales <gugomog3@gmail.com>
2023-12-27 18:16:22 -0500

    refactor: separate composables

--- app/src/main/java/com/example/mytestingapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/mytestingapp/presentation/ui/MainActivity.kt
@@ -1,169 +1,17 @@
 package com.example.mytestingapp.presentation.theme.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TextFieldButtonsAndSnackbarsCompose()
-        }
-    }
-}
-
-@Composable
-fun SimpleAnimation() {
-
-    var sizeState by remember {
-        mutableStateOf(200.dp)
-    }
-
-    val size by animateDpAsState(
-        targetValue = sizeState,
-//        animationSpec = tween(
-//            durationMillis = 3000,
-//            delayMillis = 300,
-//            easing = LinearOutSlowInEasing
-//        )
-//    )
-//        animationSpec = spring(
-//            dampingRatio = Spring.DampingRatioHighBouncy,
-//            stiffness = Spring.StiffnessLow
-//        )
-//    )
-        keyframes {
-            durationMillis = 5000
-            sizeState at 0 with LinearOutSlowInEasing
-            sizeState * 1.5f at 1000 with FastOutLinearInEasing
-            sizeState * 2f at 5000
-        }
-    )
-
-    val infiniteTransition = rememberInfiniteTransition()
-    val color by infiniteTransition.animateColor(
-        initialValue = Color.Red,
-        targetValue = Color.Green,
-        animationSpec = infiniteRepeatable(
-            tween(durationMillis = 2000),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-
-    Box(
-        modifier = Modifier
-            .size(size)
-            .background(Color.DarkGray),
-        contentAlignment = Alignment.Center
-    ) {
-        Button(
-            onClick = {
-                sizeState += 50.dp
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = color,
-                contentColor = Color.Black
-            )
-        ) {
-            Text(text = "Increase Size")
+//            TextFieldButtonsAndSnackbarsCompose()
+//            SimpleAnimation()
         }
     }
 }
 
 
-
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "RememberReturnType")
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TextFieldButtonsAndSnackbarsCompose(
-) {
-
-    val snackHostState = remember {
-        SnackbarHostState()
-    }
-
-    val scope = rememberCoroutineScope()
-
-    var textFieldState by remember {
-        mutableStateOf("")
-    }
-
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        snackbarHost = { SnackbarHost(hostState = snackHostState) }
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 30.dp)
-        ) {
-            TextField(
-                value = textFieldState,
-                onValueChange = {
-                    textFieldState = it
-                },
-                label = {
-                    Text(text = "Enter your name")
-                },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = {
-                    scope.launch {
-                        snackHostState.showSnackbar(
-                            "Hello, ${textFieldState}",
-                            duration = SnackbarDuration.Long
-                        )
-                    }
-                }
-            ) {
-                Text(text = "Please greet me")
-            }
-        }
-    }
-}
