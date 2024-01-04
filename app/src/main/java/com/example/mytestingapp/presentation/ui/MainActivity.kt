17290e1a1d0c10aca52246593c186a86c3f11655
GGomezMorales <gugomog3@gmail.com>
2024-01-04 18:36:23 -0500

    chore: add unit1 app for codelab

--- app/src/main/java/com/example/mytestingapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/mytestingapp/presentation/ui/MainActivity.kt
@@ -3,20 +3,30 @@ package com.example.mytestingapp.presentation.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import com.example.mytestingapp.presentation.ui.composables.DrawPaths
 import com.example.mytestingapp.presentation.ui.composables.LazyListsCompose
 import com.example.mytestingapp.presentation.ui.composables.ListsCompose
 import com.example.mytestingapp.presentation.ui.composables.SimpleAnimation
+import com.example.mytestingapp.presentation.ui.composables.Unit1BirthdayCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            TextFieldButtonsAndSnackbarsCompose()
-//            SimpleAnimation()
-//            ListsCompose()
-//            LazyListsCompose()
-            DrawPaths()
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+//                TextFieldButtonsAndSnackbarsCompose()
+//                SimpleAnimation()
+//                ListsCompose()
+//                LazyListsCompose()
+//                DrawPaths()
+                Unit1BirthdayCard()
+            }
+
         }
     }
 }
