7d32bff01037f938f538f7d61f59b84120954f20
GGomezMorales <gugomog3@gmail.com>
2024-01-05 17:51:51 -0500

    feat: implemented snackbar in compose

---
+++ app/src/main/java/com/example/mytestingapp/presentation/ui/composables/Snackbar.kt
@@ -0,0 +1,83 @@
+package com.example.mytestingapp.presentation.ui.composables
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "RememberReturnType")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Snackbars(
+) {
+
+    val snackHostState = remember {
+        SnackbarHostState()
+    }
+
+    val scope = rememberCoroutineScope()
+
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        snackbarHost = { SnackbarHost(hostState = snackHostState) }
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp)
+        ) {
+            TextField(
+                value = textFieldState,
+                onValueChange = {
+                    textFieldState = it
+                },
+                label = {
+                    Text(text = "Enter your name")
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {
+                    scope.launch {
+                        snackHostState.showSnackbar(
+                            "Hello, $textFieldState",
+                            duration = SnackbarDuration.Short
+                        )
+                    }
+                }
+            ) {
+                Text(text = "Please greet me")
+            }
+        }
+    }
+}
