a0f3c5476243117f58b0a4b4f03aec7253e815f3
GGomezMorales <gugomog3@gmail.com>
2024-01-05 15:45:05 -0500

    refactor: version 1.0

--- app/src/main/java/com/example/mytestingapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/mytestingapp/presentation/ui/MainActivity.kt
@@ -6,11 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.mytestingapp.presentation.ui.composables.DrawPaths
-import com.example.mytestingapp.presentation.ui.composables.LazyListsCompose
-import com.example.mytestingapp.presentation.ui.composables.ListsCompose
-import com.example.mytestingapp.presentation.ui.composables.SimpleAnimation
-import com.example.mytestingapp.presentation.ui.composables.Unit1BirthdayCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +14,8 @@ class MainActivity : ComponentActivity() {
             Surface(
                 modifier = Modifier.fillMaxSize()
             ) {
-//                TextFieldButtonsAndSnackbarsCompose()
-//                SimpleAnimation()
-//                ListsCompose()
-//                LazyListsCompose()
-//                DrawPaths()
-                Unit1BirthdayCard()
-            }
 
+            }
         }
     }
 }
